fa8a6039b669296b99b410e01aeb407479ce067e
EduardoMK <eduardokonz@gmail.com>
2024-08-26 22:43:48 -0300

    validacao nos campos de criacao de novo usuario

---
+++ src/main/java/com/eduardo/helpdesk/resources/exceptions/FieldMessage.java
@@ -0,0 +1,38 @@
+package com.eduardo.helpdesk.resources.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String fieldName;
+	private String menssage;
+
+	public FieldMessage() {
+		super();
+	}
+
+	public FieldMessage(String fieldName, String menssage) {
+		super();
+		this.fieldName = fieldName;
+		this.menssage = menssage;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMenssage() {
+		return menssage;
+	}
+
+	public void setMenssage(String menssage) {
+		this.menssage = menssage;
+	}
+
+}
