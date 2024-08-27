55ffffcb0e2e6a0815c1a908cd0da51c8e0a31d5
EduardoMK <eduardokonz@gmail.com>
2024-08-26 21:22:45 -0300

    excecao para CPF e EMAIL ja cadastrados

--- src/main/java/com/eduardo/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/eduardo/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,16 @@
 package com.eduardo.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eduardo.helpdesk.domain.Pessoa;
+import java.util.List;
+
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 
 }
--- src/main/java/com/eduardo/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/eduardo/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.eduardo.helpdesk.services.exceptions.DataIntegratyViolationException;
 import com.eduardo.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,5 +19,13 @@ public class ResourceExceptionHandler {
 				HttpStatus.NOT_FOUND.value(), "Object Not Found", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegratyViolationException.class)
+	public ResponseEntity<StandardError> dataIntegratyViolationException(DataIntegratyViolationException ex, HttpServletRequest request) {
+		StandardError error = new StandardError(System.currentTimeMillis(),
+				HttpStatus.BAD_REQUEST.value(), "Violação de dados", ex.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
